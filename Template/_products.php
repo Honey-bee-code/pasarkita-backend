82b92a5392e92f974dd5fa77c4015c6fc2868dfd
Honey-bee-code <hanifinida@gmail.com>
2021-06-03 01:22:57 +0800

    product

---
+++ Template/_products.php
@@ -0,0 +1,130 @@
+<section id="product" class="py-3">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <img src="./assets/products/sayur/sayurbayam.jpg" alt="sayurbayam" class="img-fluid" />
+                            <div class="form row pt-4 font-size-16 font-baloo">
+                                <div class="col">
+                                    <button type="submit" class="btn btn-danger form-control">Langsung Beli</button>
+                                </div>
+                                <div class="col">
+                                    <button type="submit" class="btn btn-warning form-control">Tambah ke Keranjang</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 py-5">
+                            <h4 class="font-baloo font-size-20">Sayur Bayam</h4>
+                            <small>dari Ladang Bapak Hamid</small>
+                            <div class="d-flex">
+                                <div class="rating text-warning font-size-12">
+                                    <span><i class="fas fa-star"></i></span>
+                                    <span><i class="fas fa-star"></i></span>
+                                    <span><i class="fas fa-star"></i></span>
+                                    <span><i class="fas fa-star"></i></span>
+                                    <span><i class="far fa-star"></i></span>
+                                </div>
+                                <a href="#" class="px-2 font-rale font-size-14">1.254 rating | 1000+ pertanyaan terjawab</a>
+                            </div>
+                            <hr class="m-0" />
+
+                            <!-- Product price -->
+                            <table class="my-3">
+                                <tr class="font-rale font-size-14">
+                                    <td>Harga Tertinggi</td>
+                                    <td style="text-decoration-line: line-through !important">Rp 5.000</td>
+                                </tr>
+                                <tr class="font-rale font-size-14">
+                                    <td>Harga Pas</td>
+                                    <td class="font-size-20 text-danger">Rp <span>3.000</span><small class="text-dark font-size-12">&nbsp;&nbsp;sudah termasuk pajak</small></td>
+                                </tr>
+                                <tr class="font-rale font-size-14">
+                                    <td>Anda Hemat</td>
+                                    <td>Rp <span class="font-size-16 text-danger">2.000</span></td>
+                                </tr>
+                            </table>
+                            <!-- Product price End -->
+
+                            <!-- Policy -->
+                            <div id="policy">
+                                <div class="d-flex">
+                                    <div class="return text-center me-5">
+                                        <div class="font-size-20 my-2 color-second">
+                                            <span class="fas fa-retweet border p-3 rounded-pill"></span>
+                                        </div>
+                                        <a href="#" class="font-rale font-size-12"
+                                            >1 Hari <br />
+                                            Bisa ditukar</a
+                                        >
+                                    </div>
+                                    <div class="return text-center me-5">
+                                        <div class="font-size-20 my-2 color-second">
+                                            <span class="fas fa-truck border p-3 rounded-pill"></span>
+                                        </div>
+                                        <a href="#" class="font-rale font-size-12">HoneyBeeCode<br />Pengiriman</a>
+                                    </div>
+                                    <div class="return text-center me-5">
+                                        <div class="font-size-20 my-2 color-second">
+                                            <span class="fas fa-check-double border p-3 rounded-pill"></span>
+                                        </div>
+                                        <a href="#" class="font-rale font-size-12"
+                                            >1 hari <br />
+                                            Garansi</a
+                                        >
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Policy End -->
+                            <hr />
+
+                            <!-- Order Details -->
+                            <div id="order-details" class="font-rale d-flex flex-column text-dark">
+                                <small>Pengiriman tanggal : 29 Maret - 1 April</small>
+                                <small>Dijual oleh <a href="#">Ibu Rahmi</a> (4.5 dari 5 | 1.267 rating)</small>
+                                <small><i class="fas fa-map-marker-alt color-primary"></i>&nbsp;&nbsp;Deliver to customer -4235458</small>
+                            </div>
+                            <!-- Order Details End -->
+
+                            <div class="row my-3">
+                                <div class="col">
+                                    <div class="qty d-flex">
+                                        <h6 class="font-baloo">Jumlah </h6>
+                                        <div class="px-4 d-flex font-rale">
+                                            <button data-id="pro1" class="qty-up border bg-light"><i class="fas fa-angle-up"></i></button>
+                                            <input data-id="pro1" type="text" class="qty-input border px-2 mx-2 w-50 bg-light" disabled value="1" placeholder="1" />
+                                            <button data-id="pro1" class="qty-down border bg-light"><i class="fas fa-angle-down"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Size -->
+                            <!-- <div class="size my-3">
+                                <h6 class="font-baloo">Satuan</h6>
+                                <div class="d-flex justify-content-between w-75">
+                                    <div class="font-rubik border p-2">
+                                        <button class="btn p-0 font-size-14">Kilogram</button>
+                                    </div>
+                                    <div class="font-rubik border p-2">
+                                        <button class="btn p-0 font-size-14">Gram</button>
+                                    </div>
+                                    <div class="font-rubik border p-2">
+                                        <button class="btn p-0 font-size-14">Ons</button>
+                                    </div>
+                                    <div class="font-rubik border p-2">
+                                        <button class="btn p-0 font-size-14">Ikat</button>
+                                    </div>
+                                </div>
+                            </div> -->
+                            <!-- Size End -->
+                        </div>
+                    </div>
+                    <div class="row py-5">
+                        <div class="col-12">
+                            <h6 class="font-rubik">Deskripsi Produk</h6>
+                            <hr>
+                            <p class="font-rale font-size-14">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum id molestiae aspernatur ducimus harum omnis reiciendis tenetur similique ullam libero, corporis fugiat alias unde, nemo veritatis ab. Ut illo animi vero quisquam ab similique, distinctio maiores et, error dicta repellendus harum repellat enim nihil accusamus dolore officia! Sequi ipsa, cumque provident ullam, illum perferendis saepe minima, eos tempore quod facilis?</p>
+                            <p class="font-rale font-size-14">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum id molestiae aspernatur ducimus harum omnis reiciendis tenetur similique ullam libero, corporis fugiat alias unde, nemo veritatis ab. Ut illo animi vero quisquam ab similique, distinctio maiores et, error dicta repellendus harum repellat enim nihil accusamus dolore officia! Sequi ipsa, cumque provident ullam, illum perferendis saepe minima, eos tempore quod facilis?</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
