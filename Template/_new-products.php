5612115df42ead939a747106db0ee482d1646cd5
Honey-bee-code <hanifinida@gmail.com>
2021-06-07 20:14:38 +0800

    shuffle products

--- Template/_new-products.php
+++ Template/_new-products.php
@@ -1,5 +1,7 @@
 
 <!-- New Products -->
+
+
 <section id="new-products">
     <div class="container">
         <h4 class="font-rubik font-size-20">Produk Terbaru</h4>
@@ -7,11 +9,12 @@
 
         <!-- owl carousel -->
         <div class="owl-carousel owl-theme">
+        <?php foreach($product_shuffle as $item) { ?>
             <div class="item py-2 px-1">
                 <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/buah/mangga.jpeg" alt="mangga" class="img-fluid" /></a>
+                    <a href="#"><img src="<?= $item['item_image'] ?  : './assets/products/product.jpg' ?>" alt="product image" class="img-fluid" /></a>
                     <div class="text-center">
-                        <h6>Mangga</h6>
+                        <h6><?= $item['item_name'] ? : 'Tanpa Nama' ?></h6>
                         <div class="rating text-warning font-size-12">
                             <span><i class="fas fa-star"></i></span>
                             <span><i class="fas fa-star"></i></span>
@@ -20,107 +23,13 @@
                             <span><i class="far fa-star"></i></span>
                         </div>
                         <div class="price py-2">
-                            <span>Rp 15.000</span>
+                            <span>Rp <?= $item['item_price'] ? : '0' ?></span>
                         </div>
                         <button type="submit" class="btn btn-warning font-size-12">Tambah ke Keranjang</button>
                     </div>
                 </div>
             </div>
-            <div class="item py-2 px-1">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/buah/apel.jpg" alt="apel" class="img-fluid" /></a>
-                    <div class="text-center">
-                        <h6>Apel</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>Rp 40.000</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Tambah ke Keranjang</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item py-2 px-1">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/buah/jeruk.jpg" alt="jeruk" class="img-fluid" /></a>
-                    <div class="text-center">
-                        <h6>Jeruk</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>Rp 25.000</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Tambah ke Keranjang</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item py-2 px-1">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/sayur/sayurkol.jpeg" alt="sayurkol" class="img-fluid" /></a>
-                    <div class="text-center">
-                        <h6>Sayur Kol</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>Rp 5.000</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Tambah ke Keranjang</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item py-2 px-1">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/sayur/sayurbayam.jpg" alt="sayurbayam" class="img-fluid" /></a>
-                    <div class="text-center">
-                        <h6>Sayur Bayam</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>Rp 3.000</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Tambah ke Keranjang</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item py-2 px-1">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/daging/ayampotong.jpg" alt="ayampotong" class="img-fluid" /></a>
-                    <div class="text-center">
-                        <h6>Ayam Potong</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>Rp 35.000</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Tambah ke Keranjang</button>
-                    </div>
-                </div>
-            </div>
+            <?php } ?>            
         </div>
         <!-- owl carousel end -->
     </div>
